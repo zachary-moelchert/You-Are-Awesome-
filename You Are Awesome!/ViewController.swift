41d2490a638f3a15e949002b1555609863657f9d
Zachary Moelchert <zachary.moelchert@bc.edu>
2021-02-06 14:43:08 -0500

    2.8  - f / Else If / Else

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,13 +14,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
-       
+        
     }
-
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        } else  {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+            
+        }
+        
     }
     
 }
-
