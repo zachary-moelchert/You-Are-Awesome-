8dbe4d8b9da56597e3ca76cbc628662070f46e2d
Zachary Moelchert <zachary.moelchert@bc.edu>
2021-02-07 12:17:05 -0500

    2.11 - Random messages and images

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,43 +25,54 @@ class ViewController: UIViewController {
         var messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fabulous!",
-                        "You Da Bomb!"]
+                        "When The Genius Bar Needs Help, They Call You!",
+                        "You Da Bomb!",
+                        "You've Got The Design Skills of Jony Ive"]
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
-            
+        // Using a counter to cycle through messages
+        //        messageLabel.text = messages[messageNumber]
+        //        messageNumber += 1
+        //        if messageNumber == messages.count {
+        //            messageNumber = 0
         
-        print(imageNumber)
-       // let imageName = "image" + String(imageNumber)
-        let imageName = "image\(imageNumber)"
-        imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-            
-        }
+        // Getting a random message and image using .random
+       
+        messageLabel.text = messages[Int.random(in: 0...messages.count - 1)]
+        let imageName = "image\(Int.random(in: 0...totalNumberOfImages))"
+            imageView.image = UIImage(named: imageName)
         
-       // imageView.image = UIImage(named: "image")
-        
-                //        let awesomeMessage = "You Are Awesome!"
-                //        let greatMessage = "You Are Great!"
-                //        let bombMessage = "You Are Da Bomb!"
-                //
-                //        if messageLabel.text == awesomeMessage {
-                //            messageLabel.text = greatMessage
-                //            imageView.image = UIImage(named: "image1")
-                //        } else if messageLabel.text == greatMessage {
-                //            messageLabel.text = bombMessage
-                //            imageView.image = UIImage(named: "image2")
-                //        } else  {
-                //            messageLabel.text = awesomeMessage
-                //            imageView.image = UIImage(named: "image0")
-                //
-                //        }
-        
-    }
+    
+    // Using If statements to cycle through images
+    //        print(imageNumber)
+    //        // let imageName = "image" + String(imageNumber)
+    //        let imageName = "image\(imageNumber)"
+    //        imageView.image = UIImage(named: imageName)
+    //        imageNumber = imageNumber + 1
+    //        if imageNumber == 10 {
+    //        imageNumber = 0
+    //
+    
     
 }
+
+// imageView.image = UIImage(named: "image")
+
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else  {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//
+//        }
+
+}
+
+
